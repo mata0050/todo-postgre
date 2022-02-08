fffd33b60f7c396bfd4a904ee66f7e7a1ba78a03
mata0050 <mata0050@outlook.com>
2022-02-08 18:23:02 -0500

    all routes working

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
-const { urlencoded } = require('express');
 const express = require('express');
 const { errorHandler } = require('./middleware/errorMiddleware');
 const app = express();
 
 // Middleware
 app.use(express.json());
-app.use(urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: false }));
 
 // Routes
 app.use('/api/notes', require('./routes/note'));
 
 // Error Middleware
 app.use(errorHandler);
+
 const PORT = process.env.PORT || 7000;
 app.listen(PORT, () => {
   console.log(`Server is listening at http://localhost:${PORT}`);
