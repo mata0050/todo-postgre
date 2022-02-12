26439a6a0c12d638fb18576d245aee059d2acf88
mata0050 <mata0050@outlook.com>
2022-02-12 18:56:22 -0500

    add note and get note function added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,17 +6,38 @@ function App() {
   const [note, setNote] = useState('');
   const [notes, setNotes] = useState([]);
 
-  useEffect(() => {
-    const fetchNotes = async () => {
+  const fetchNotes = async () => {
+    try {
       const { data } = await axios.get('/api/note');
       setNotes(data);
-    };
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
     fetchNotes();
   }, []);
+
+  const addPost = async () => {
+    try {
+      const res = await axios.post('/api/note', { note: note });
+      console.log(res);
+      fetchNotes();
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    addPost();
+  };
+
   console.log(notes);
   return (
     <div className='App'>
-      <form action='submit'>
+      <form action='submit' onSubmit={onSubmit}>
         <label className='block' htmlFor='note'>
           Add Note
         </label>
@@ -28,9 +49,14 @@ function App() {
           id='note'
         />
       </form>
+
+      <main>
+        {notes.map((note) => (
+          <p key={note.id}>{note.note}</p>
+        ))}
+      </main>
     </div>
   );
 }
 
 export default App;
-
